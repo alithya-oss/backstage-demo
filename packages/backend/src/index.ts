1b3c57cec842b98cf330023849d6fc79db9c2cd1
Florian JUDITH <florian.judith@alithya.com>
2025-05-20 00:51:48 -0400

    Added support for Backstage internal OpenAPI doc

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -52,4 +52,9 @@ backend.add(import('@backstage/plugin-search-backend-module-techdocs'));
 // kubernetes
 backend.add(import('@backstage/plugin-kubernetes-backend'));
 
+// backstage openapi generator
+backend.add(
+  import('@backstage/plugin-catalog-backend-module-backstage-openapi'),
+);
+
 backend.start();
